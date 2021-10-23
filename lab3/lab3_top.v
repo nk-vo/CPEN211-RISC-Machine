fb565240d97bac287df32b26d8a1c43ab9571ec1
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2021-10-22 18:37:52 -0700

    first commit

---
+++ lab3/lab3_top.v
@@ -0,0 +1,8 @@
+module lab3_top(SW,KEY,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,LEDR);
+  input [9:0] SW;
+  input [3:0] KEY;
+  output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+  output [9:0] LEDR; // optional: use these outputs for debugging on your DE1-SoC
+
+  // put your solution code here!
+endmodule
