c4ead8aedf63fdf81fbcef69fd21d2f026508983
oxygen-is-toxic <71479562+oxygen-is-toxic@users.noreply.github.com>
2021-10-27 01:00:10 -0700

    basically new

--- lab3/lab3_top.v
+++ lab3/lab3_top.v
@@ -12,17 +12,22 @@
 `define b9  4'b1001
 
 // states corresponds to 6 last ID digits and open/close/error
-`define S0    4'b0000   // start state
-`define S1    4'b0001   // 3
-`define S2    4'b0010   // 0
-`define S3    4'b0011   // 5
-`define S4    4'b0100   // 4
-`define S5    4'b0101   // 6
-`define S6    4'b0110   // 4
-`define open  4'b0111   // open
-`define close 4'b1000   // close
-`define error 4'b1001   // error
-`define base  4'bxxxx   // base case
+`define valid_1   4'b0000   // 3
+`define valid_2   4'b0001   // 0
+`define valid_3   4'b0010   // 5
+`define valid_4   4'b0011   // 4
+`define valid_5   4'b0100   // 6
+`define valid_6   4'b0101   // 4
+`define invalid_1 4'b0110   // invalid state 1
+`define invalid_2 4'b0111   // invalid state 2
+`define invalid_3 4'b1000   // invalid state 3
+`define invalid_4 4'b1001   // invalid state 4
+`define invalid_5 4'b1010   // invalid state 5
+`define open      4'b1011   // open
+`define close     4'b1100   // close
+`define error     4'b1101   // error
+
+
 
 // HEXCODE 0-9
 `define h0      7'b1_000_000
@@ -45,72 +50,66 @@ module lab3_top(SW,KEY,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,LEDR);
   output reg [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
   output [9:0] LEDR; // optional: use these outputs for debugging on your DE1-SoC
 
-
-  reg [3:0] present_state;
   // put your solution code here!
-  
-  always @(posedge ~KEY[0]) begin
-    if (~KEY[3]) begin
-      present_state = `S1;
-      HEX0 = 7'bx_xxx_xxx;
-    end
+  reg [3:0]  next_state;
+  wire [3:0] present_state, next_state_reset;
 
+  vDFF #(4) vdff_1(~KEY[0], next_state_reset, present_state);
+  assign next_state_reset = ~KEY[3] ? `valid_1 : next_state;
+
+  //Dec dec(SW,next_state,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5); 
+  
+  always @(*) begin
+    if (SW[3:0] < `b0 | SW[3:0] > `b9)  {HEX4,HEX3,HEX2,HEX1,HEX0} = `ErrOr;
     else begin
       case (present_state)
-        `S1:  if (SW[3:0] == `b3)
-                present_state = `S2;
-              else
-                present_state = `error;
-        `S2:  if (SW[3:0] == `b0)
-                present_state = `S3;
-              else
-                present_state = `error;
-        `S3:  if (SW[3:0] == `b5)
-                present_state = `S4;
-              else
-                present_state = `error;
-        `S4:  if (SW[3:0] == `b4)
-                present_state = `S5;
-              else
-                present_state = `error;
-        `S5:  if (SW[3:0] == `b6)
-                present_state = `S6;
-              else
-                present_state = `error;
-        `S6:  if (SW[3:0] == `b4)
-                present_state = `open;
-              else
-                present_state = `error;
-        `error: begin 
-                  present_state = `close;
-                end
-        `close: begin
-                  present_state = `close;               
-                end
-        `open:  begin
-                  present_state = `open;
-                end
-        default: present_state = 4'bxxxx;
+        `valid_1:   {next_state, HEX0} = {(SW[3:0] == `b3) ? `valid_2 : `invalid_1, `b3};
+        `valid_2:   {next_state, HEX0} = {(SW[3:0] == `b0) ? `valid_3 : `invalid_2, `b0};
+        `valid_3:   {next_state, HEX0} = {(SW[3:0] == `b5) ? `valid_4 : `invalid_3, `b5};
+        `valid_4:   {next_state, HEX0} = {(SW[3:0] == `b4) ? `valid_5 : `invalid_4, `b4};
+        `valid_5:   {next_state, HEX0} = {(SW[3:0] == `b6) ? `valid_6 : `invalid_5, `b6};
+        `valid_6:   {next_state, HEX0} = {(SW[3:0] == `b4) ? `open    : `close,     `b4};
+        `invalid_1: next_state = `invalid_2;
+        `invalid_2: next_state = `invalid_3;
+        `invalid_3: next_state = `invalid_4;
+        `invalid_4: next_state = `invalid_5;
+        `invalid_5: next_state = `close;
+        `close:     begin
+                      next_state = `close;
+                      {HEX5,HEX4,HEX3,HEX2,HEX1,HEX0} = `CLOSEd;
+                    end
+        `open:      begin
+                      next_state = `open;
+                      {HEX3,HEX2,HEX1,HEX0} = `OPEn;
+                    end
+        default:    next_state = 4'bxxxx;
       endcase
     end
-    if (SW[3:0] < `b0 | SW[3:0] > `b9) begin
-      present_state = `error;
-    end
-    case ({SW[3:0],present_state})
-      {`b3,`S1}: HEX0 = `h3;
-      {`b0,`S2}: HEX0 = `h0;
-      {`b5,`S3}: HEX0 = `h5;
-      {`b4,`S4}: HEX0 = `h4;
-      {`b5,`S5}: HEX0 = `h6;
-      {`b4,`S6}: HEX0 = `h4;
-      {`base,`error}: {HEX4,HEX3,HEX2,HEX1,HEX0} = `ErrOr;
+  end
+endmodule
+
+module vDFF(clk, in, out);
+  parameter n = 1;
+  input clk;
+  input [n-1:0] in;
+  output reg [n-1:0] out;
+  always @(posedge clk) begin
+    out <= in;
+  end
+endmodule
+
+/*module Dec(SW,next_state,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
+  
+  case ({SW[3:0],present_state})
+      {4'bx,`valid_1}:HEX0 = `h3;
+      {`b3,`valid_2}: HEX0 = `h3;
+      {`b0,`valid_2}: HEX0 = `h0;
+      {`b5,`valid_3}: HEX0 = `h5;
+      {`b4,`valid_4}: HEX0 = `h4;
+      {`b5,`valid_5}: HEX0 = `h6;
+      {`b4,`valid_6}: HEX0 = `h4;
       {`base,`close}: {HEX5,HEX4,HEX3,HEX2,HEX1,HEX0} = `CLOSEd;
       {`base,`open}:  {HEX3,HEX2,HEX1,HEX0} = `OPEn;
       default: {HEX0, HEX1, HEX2, HEX3, HEX4, HEX5} ={6{7'bx}};
     endcase
-  end
-endmodule
-
-module Dec();
-  
-endmodule
+endmodule*/
--- lab3/lab3_top_tb.v
+++ lab3/lab3_top_tb.v
@@ -12,17 +12,20 @@
 `define b9  4'b1001
 
 // states corresponds to 6 last ID digits and open/close/error
-`define S0    4'b0000   // start state
-`define S1    4'b0001   // 3
-`define S2    4'b0010   // 0
-`define S3    4'b0011   // 5
-`define S4    4'b0100   // 4
-`define S5    4'b0101   // 6
-`define S6    4'b0110   // 4
-`define open  4'b0111   // open
-`define close 4'b1000   // close
-`define error 4'b1001   // error
-`define base  4'bxxxx   // base case
+`define valid_1   4'b0000   // 3
+`define valid_2   4'b0001   // 0
+`define valid_3   4'b0010   // 5
+`define valid_4   4'b0011   // 4
+`define valid_5   4'b0100   // 6
+`define valid_6   4'b0101   // 4
+`define invalid_1 4'b0110   // invalid state 1
+`define invalid_2 4'b0111   // invalid state 2
+`define invalid_3 4'b1000   // invalid state 3
+`define invalid_4 4'b1001   // invalid state 4
+`define invalid_5 4'b1010   // invalid state 5
+`define open      4'b1011   // open
+`define close     4'b1100   // close
+`define error     4'b1101   // error
 
 // HEXCODE 0-9
 `define h0      7'b1_000_000
@@ -49,6 +52,21 @@ module lab3_top_tb;
 
     lab3_top dut(SW, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, LEDR);
 
+    task my_checker;
+        input [4:0] expected_state;
+        input [6:0] expected_hex0;
+    begin
+        if (lab3_top_tb.dut.present_state !== expected_state) begin
+            $display("ERROR ** state is %b, expected %b", lab3_top_tb.dut.present_state, expected_state);
+            err = 1'b1;
+        end
+        if (lab3_top_tb.dut.HEX0 !== expected_hex0) begin
+            $display("ERROR ** HEX0 is %b, expected %b", lab3_top_tb.dut.HEX0, expected_hex0);
+            err = 1'b1;
+        end
+    end
+    endtask
+
     initial begin
         KEY[0] = 1; #5;
         forever begin
@@ -58,33 +76,21 @@ module lab3_top_tb;
     end
 
     initial begin
-        KEY[3] = 1'b0;
-        SW[3:0] = 4'b0000;
-        err = 1'b0;
-        #10;
-        if (HEX0 !== 7'bx_xxx_xxx) begin
-            $display("ERROR ** output is %b, expected %b", HEX0, 7'bx_xxx_xxx);
-            err = 1'b1;
-        end
-
+        $display("checking reset");
+        KEY[3] = 1'b0; err = 1'b0; #10;
+        my_checker(`valid_1, `h3);
         KEY[3] = 1'b1;  // release reset
+
         $display("checking S1->S2");
-        SW[3:0] = `b3;
-        if (HEX0 !== `h3) begin
-            $display("ERROR ** output is %b, expected %b", HEX0, `h3);
-            err = 1'b1;
-        end
-        #10;
+        SW[3:0] = `b3; #10;
+        my_checker(`valid_2, `h0);
 
         $display("chekcing S2->S3");
-        SW[3:0] = `b0;
-        if (HEX0 !== `h0) begin
-            $display("ERROR ** output is %b, expected %b", HEX0, `h0);
-            err = 1'b1;
-        end
-        #10;
+        SW[3:0] = `b0; #10;
+        my_checker(`valid_3, `h5);
+        
 
-        $display("chekcing S2->S3");
+        $display("chekcing S3->S4");
         SW[3:0] = `b5;
         if (HEX0 !== `h5) begin
             $display("ERROR ** output is %b, expected %b", HEX0, `h5);
@@ -92,7 +98,7 @@ module lab3_top_tb;
         end
         #10;
 
-        $display("chekcing S2->S3");
+        $display("chekcing S4->S5");
         SW[3:0] = `b4;
         if (HEX0 !== `h4) begin
             $display("ERROR ** output is %b, expected %b", HEX0, `h4);
@@ -100,7 +106,7 @@ module lab3_top_tb;
         end
         #10;
 
-        $display("chekcing S2->S3");
+        $display("chekcing S5->S6");
         SW[3:0] = `b6;
         if (HEX0 !== `h0) begin
             $display("ERROR ** output is %b, expected %b", HEX0, `h6);
@@ -115,8 +121,9 @@ module lab3_top_tb;
             err = 1'b1;
         end
         #10;
-        
+
         if (~err) $display("PASSED");
+        else $display("FAILED");
         $stop;
     end
 endmodule
